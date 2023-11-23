4671fe296fdfe135dd18e10d30bc1eabafc38c59
Smit Kukadiya <71758280+smit-kukadiya@users.noreply.github.com>
2023-11-23 12:54:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
    name='aso',
    version='0.1',
    description='App Store Optimization (ASO)',
+   long_description=long_description,
+   long_description_content_type="text/markdown",
    author='smit-kukadiya',
    author_email='smitk85143@gmail.com',
    packages=setuptools.find_packages(),
    url="https://github.com/smit-kukadiya/aso.git",
+   classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+   python_requires='>=3.9',
    entry_points={
         'console_scripts': [
             'aso=aso.__main__:main'
